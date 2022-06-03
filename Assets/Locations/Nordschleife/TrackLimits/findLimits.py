60a59f4f536cd4cb7187c8ca288ff6c37aa92ad6
Lachdown <lachlanb@outlook.co.nz>
2022-06-03 18:21:48 +1200

    K-means clustering for limit-finding

--- Assets/Locations/Nordschleife/TrackLimits/findLimits.py
+++ Assets/Locations/Nordschleife/TrackLimits/findLimits.py
@@ -34,6 +34,12 @@ def plotTrack(*args):
 
     pass
 
+def calcDist(p1, p2):
+
+    dist = (p2-p1)**2
+
+    return np.sum(dist)
+
 def findTrackLimits(track, s = 10):
     
     track_vertices = track[:,[0,2]]
@@ -41,28 +47,44 @@ def findTrackLimits(track, s = 10):
 
     n = int(np.ceil(shape/s))
 
-    #obsolete with k-means
-    # inds = np.arange(n)*s
-    # inds = np.append(inds, track_vertices.shape[0])
+    kmeans = KMeans(nclusters = n, random_state = 0).Fit(track_vertices)
 
-    # obsolete with K-means
-    # avg_points = [np.mean(track_vertices[start:end], axis=0) for start, end in zip(inds[:-1], inds[1:])]
-    # avg_points = np.array(avg_points)
+    #find closest 2 clusters
+    closest = np.zeros(n,2)
+    for i in range(n):
+        max_dist1 = np.inf
+        max_dist2 = np.inf
+        max_1 = i-1
+        max_2 = i+1
 
-    ii = [0] + [i for i in range(n-1)]
-    jj = [i for i in range(1,n)] + [n-1]
-    ii = np.array(ii); jj = np.array(jj)
+        p1 = kmeans.cluster_centers_[i]
+        for j in range(n):
+            if i == j:
+                continue
+            p2 = kmeans.cluster_centers_[j]
 
-    m = (avg_points[ii,1] - avg_points[jj,1]) / (avg_points[ii,0] - avg_points[jj,0])
+            dist = calcDist(p1,p2)
 
-    b = [track_vertices[start:end] - avg_points[i] for i, (start, end) in enumerate(zip(inds[:-1], inds[1:]))]
-    b = np.array([coord for bco in b for coord in bco])
+            if dist < max_dist1:
+                max_dist2 = max_dist1
+                max_dist1 = dist
+                max_2 = max_1
+                max_1 = j
+            elif dist < max_dist2:
+                max_dist2 = dist
+                max_2 = j
+        
+        closest[i] = [max_1, max_2]
+
+    m = (kmeans.cluster_centers_[closest[:,0],1] - kmeans.cluster_centers_[closest[:,1],1]) / (kmeans.cluster_centers_[closest[:,0],0] - kmeans.cluster_centers_[closest[:,1],0])
+
+    b = track_vertices[kmeans.labels_] - kmeans.cluster_centers_
 
     a = np.zeros((2,2))
-    z = np.zeros(b.shape[0])
+    z = np.zeros(track_vertices.shape[0])
 
     #not enough memory for a sparse array
-    for i, (start, end) in enumerate(zip(inds[:-1], inds[1:])):
+    for i in range(n):
 
         #lets hope this works
         mag = np.sqrt(1+m[i]**2)
@@ -72,22 +94,31 @@ def findTrackLimits(track, s = 10):
         a[:,0] = parr[::-1]
         a[:,1] = perp[::-1]
 
-        for j in range(start, end):
+        temp_z = z[kmeans.labels_ == i]
 
-            z[j] = np.linalg.solve(a,b[j])[1]
+        #find z distance
+        for j in len(kmeans.labels_ == i):
+            temp_z[j] = np.linalg.solve(a,b[j])[1]
 
-    sides = [[start + np.argmin(z[start:end]), start+np.argmax(z[start:end])] for start, end in zip(inds[:-1], inds[1:])]
-    sides = np.array(sides).T
+        z[kmeans.labels_ == i] = temp_z
 
-    return track[sides]
+    sides = np.zeros((n,2))
+
+    #find positions of extreme points in group
+    for i in range(n):
+
+        masked_z1 = np.ma.array(z, mask=(kmeans.labels_==i), fill_value=np.inf)
+        masked_z2 = np.ma.array(z, mask=(kmeans.labels_==i), fill_value=-np.inf)
+
+        sides[n] = [np.argmin(masked_z1), np.argmax(masked_z2)]
+
+    return track[sides.T]
+
 
 if __name__ == "__main__":
 
-
     track, autopilot = loadTrackData((2,9), "nordschleife" + " unfinished.csv")
 
-    track_ordered = orderTrack(track)
-
-    side1, side2 = findTrackLimits(track_ordered)
+    side1, side2 = findTrackLimits(track)
 
     plotTrack(side1, side2)
