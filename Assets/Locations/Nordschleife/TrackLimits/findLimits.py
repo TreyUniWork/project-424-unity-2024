5c80093d0e759b094b23dabcc3af423d7702e6ef
Lachdown <lachlanb@outlook.co.nz>
2022-06-04 14:44:33 +1200

    KMeans method complete - flawed

--- Assets/Locations/Nordschleife/TrackLimits/findLimits.py
+++ Assets/Locations/Nordschleife/TrackLimits/findLimits.py
@@ -9,6 +9,7 @@ def loadTrackData(track_files, autopilot_data_filename):
 
     segments = [np.genfromtxt("ROAD_GR"+n+".csv", dtype=None, delimiter=',') for n in nums]
     track = np.concatenate(segments, dtype=float)
+    track = np.unique(track, axis=0)
 
     return track, None
 
@@ -23,7 +24,7 @@ def plotTrack(*args):
 
     for i, arg in enumerate(args):
 
-        ax.scatter(arg[:,0], arg[:,2], color = colors[i % len(colors)], s=1e0)
+        ax.scatter(arg[:100,0], arg[:100,2], color = colors[i % len(colors)], s=1e0)
 
     plt.title("Nordschleife")
     plt.tight_layout()
@@ -32,53 +33,32 @@ def plotTrack(*args):
 
     plt.show()
 
-    pass
 
-def calcDist(p1, p2):
+def createClusters(track, track_name, s = 20):
 
-    dist = (p2-p1)**2
+    track_vertices = track[:,[0,2]]
+    n = int(np.ceil(track_vertices.shape[0]/s))
 
-    return np.sum(dist)
+    kmeans = KMeans(n_clusters = n, random_state = 0).fit(track_vertices)
 
-def findTrackLimits(track, s = 10):
+    np.savetxt("Kmeans_labels_" + track_name + ".csv", kmeans.labels_, delimiter=',', newline='\n')
+    np.savetxt("Kmeans_centers_" + track_name + ".csv", kmeans.cluster_centers_, delimiter=',', newline='\n')
+
+def findTrackLimits(track, track_name):
     
     track_vertices = track[:,[0,2]]
-    shape = track_vertices.shape[0]
 
-    n = int(np.ceil(shape/s))
+    labels = np.genfromtxt("Kmeans_labels_" + track_name + ".csv", delimiter=',', dtype=int)
+    centers = np.genfromtxt("Kmeans_centers_" + track_name + ".csv", delimiter=',')
 
-    kmeans = KMeans(nclusters = n, random_state = 0).Fit(track_vertices)
+    n = centers.shape[0]
 
-    #find closest 2 clusters
-    closest = np.zeros(n,2)
-    for i in range(n):
-        max_dist1 = np.inf
-        max_dist2 = np.inf
-        max_1 = i-1
-        max_2 = i+1
+    closest = closestTwo(centers, n).astype(int)
 
-        p1 = kmeans.cluster_centers_[i]
-        for j in range(n):
-            if i == j:
-                continue
-            p2 = kmeans.cluster_centers_[j]
+    m = (centers[closest[:,0],1] - centers[closest[:,1],1]) / (centers[closest[:,0],0] - centers[closest[:,1],0])
 
-            dist = calcDist(p1,p2)
-
-            if dist < max_dist1:
-                max_dist2 = max_dist1
-                max_dist1 = dist
-                max_2 = max_1
-                max_1 = j
-            elif dist < max_dist2:
-                max_dist2 = dist
-                max_2 = j
-        
-        closest[i] = [max_1, max_2]
-
-    m = (kmeans.cluster_centers_[closest[:,0],1] - kmeans.cluster_centers_[closest[:,1],1]) / (kmeans.cluster_centers_[closest[:,0],0] - kmeans.cluster_centers_[closest[:,1],0])
-
-    b = track_vertices[kmeans.labels_] - kmeans.cluster_centers_
+    b = [track_vertices[labels==lab] - centers[lab] for lab in range(n)]
+    b = np.array([col for row in b for col in row])
 
     a = np.zeros((2,2))
     z = np.zeros(track_vertices.shape[0])
@@ -94,31 +74,61 @@ def findTrackLimits(track, s = 10):
         a[:,0] = parr[::-1]
         a[:,1] = perp[::-1]
 
-        temp_z = z[kmeans.labels_ == i]
+        len_label = labels[labels==i].shape[0]
+
+        temp_z = np.zeros(len_label)
+        temp_b = b[labels == i]
 
         #find z distance
-        for j in len(kmeans.labels_ == i):
-            temp_z[j] = np.linalg.solve(a,b[j])[1]
+        for j in range(len_label):
+            temp_z[j] = np.linalg.solve(a,temp_b[j])[1]
 
-        z[kmeans.labels_ == i] = temp_z
+        z[labels == i] = temp_z
 
     sides = np.zeros((n,2))
 
     #find positions of extreme points in group
     for i in range(n):
 
-        masked_z1 = np.ma.array(z, mask=(kmeans.labels_==i), fill_value=np.inf)
-        masked_z2 = np.ma.array(z, mask=(kmeans.labels_==i), fill_value=-np.inf)
+        masked_z1 = np.ma.array(z, mask=(labels!=i), fill_value=np.inf)
+        masked_z2 = np.ma.array(z, mask=(labels!=i), fill_value=-np.inf)
 
-        sides[n] = [np.argmin(masked_z1), np.argmax(masked_z2)]
+        sides[i] = [np.argmin(masked_z1), np.argmax(masked_z2)]
 
-    return track[sides.T]
+    return track[sides.astype(int).T]
+
+def closestTwo(centers, n):
+
+    #find closest 2 clusters
+    closest = np.zeros((n,2))
+
+    for i in range(n):
+
+        dists = np.sum(np.square(centers - centers[i]), axis=1)
+
+        mask = np.zeros(n)
+        mask[i] = 1
+
+        dists_masked = np.ma.array(dists, mask=mask, fill_value=np.inf)
+        closest1 = np.argmin(dists_masked)
+
+        mask[closest1] = 1
+        dists_masked = np.ma.array(dists, mask=mask, fill_value=np.inf)
+
+        closest2 = np.argmin(dists_masked)
+
+        closest[i] = [closest1, closest2]
+
+    return closest
 
 
 if __name__ == "__main__":
 
-    track, autopilot = loadTrackData((2,9), "nordschleife" + " unfinished.csv")
+    track_name = "nordschleife"
+    track, autopilot = loadTrackData((2,9), track_name + " unfinished.csv")
 
-    side1, side2 = findTrackLimits(track)
+    # createClusters(track, track_name)
 
-    plotTrack(side1, side2)
+    sides = findTrackLimits(track, track_name)
+
+    plotTrack(*sides)
