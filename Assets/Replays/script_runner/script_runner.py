40a2b50acf8dcacdabe70730d48569ec3d5b5890
llwong03 <lachlan.ljwong@gmail.com>
2023-09-01 11:34:51 +1200

    fixed parameter limits

--- Assets/Replays/script_runner/script_runner.py
+++ Assets/Replays/script_runner/script_runner.py
@@ -1,104 +1,100 @@
+import time
 import tkinter as tk
 from tkinter import filedialog
 import os
 import random
+import watchdog.observers
+import watchdog.events
 import csv
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
 
-# Define the genetic algorithm parameters
+# Genetic algorithm parameters
 num_children = 5
-num_generations = 4
 param_names = ["speed", "rawThrottle", "rawBrake", "brakePressure", "throttle"]
 
+def modify_file_content(base_content):
+    lines = base_content.split("\n")
+    modified_content = []
 
-class CSVHandler(FileSystemEventHandler):
-    def __init__(self, observer):
-        self.observer = observer
+    for line in lines:
+        line = line.strip()
+        if line.startswith("- speed"):
+            value = float(line.split(":")[1].strip())
+            new_value = value * (1 + random.uniform(-0.2, 0.2))
+            new_value = max(0, min(100, new_value))
+            modified_content.append(f"  - speed: {new_value:.6f}")
+        elif any(line.strip().startswith(param) for param in param_names):
+            param, value = line.split(":")[0].strip(), float(line.split(":")[1].strip())
+            new_value = value * (1 + random.uniform(-0.2, 0.2))
 
-    def on_created(self, event):
-        if event.is_directory:
-            return
-        elif event.src_path.endswith('.csv'):
-            print(f"Detected new CSV: {event.src_path}")
-            self.observer.stop()
+            if param == "rawThrottle" or param == "rawBrake":
+                new_value = int(max(0, min(10000, new_value)))
+            elif param == "throttle":
+                new_value = max(0, min(100, new_value))
+            elif param == "brakePressure":
+                new_value = max(0, min(1, new_value))
 
+            modified_content.append(f"    {param}: {new_value:.6f}")
+        else:
+            modified_content.append(line)
+
+    return "\n".join(modified_content)
+
+def generate_next_generation(base_file, generation):
+    base_content = load_base_input_file(base_file)
+    gen_folder = os.path.join(os.path.dirname(base_file), f"generation_{generation}")
+    os.makedirs(gen_folder, exist_ok=True)
+
+    for child in range(num_children):
+        modified_content = modify_file_content(base_content)
+        with open(os.path.join(gen_folder, f"generation{generation}_modified{child}.asset"), 'w') as file:
+            file.write(modified_content)
 
 def load_base_input_file(file_path):
-    print(f"Loading base input file from: {file_path}")
     with open(file_path, 'r') as file:
-        content = file.read()
-    return content
+        return file.read()
 
+def on_created(event):
+    if event.src_path.endswith('.csv'):
+        print(f"Detected new CSV: {event.src_path}")
+        best_two_files = select_best_times(event.src_path)
+        print(f"Best two files from CSV: {best_two_files}")
 
-def read_csv_laptimes(file_path):
-    lap_times = {}
-    with open(file_path, 'r') as csv_file:
-        reader = csv.reader(csv_file)
-        next(reader)  # Skip header row
-        for row in reader:
-            filename, laptime_str = row[0], row[1]
-            seconds, milliseconds = map(int, laptime_str.split("."))
-            laptime = seconds + milliseconds * 0.001
-            lap_times[filename] = laptime
-    return lap_times
+def select_best_times(csv_file):
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file)
+        times = [(row[0], float(row[1])) for row in reader]
+    times.sort(key=lambda x: x[1])
+    return times[:2]
 
-
-
-
-def watch_for_csv(directory):
-    print(f"Watching for CSV files in: {directory}")
-    observer = Observer()
-    event_handler = CSVHandler(observer)
-    observer.schedule(event_handler, directory, recursive=False)
-    observer.start()
-    observer.join()
-
+class MyHandler(watchdog.events.FileSystemEventHandler):
+    def on_created(self, event):
+        on_created(event)
 
 def run_genetic_algorithm():
-    base_input_file = input_file_entry.get()
-    print(f"Starting the genetic algorithm with base input file: {base_input_file}")
-    base_input_content = load_base_input_file(base_input_file)
-    lines = base_input_content.split("\n")
-    script_location = os.path.dirname(os.path.abspath(__file__))
+    base_file = input_file_entry.get()
+    generation = 0
 
-    for generation in range(num_generations):
-        print(f"Generating children for generation {generation}")
-        output_folder = os.path.join(script_location, f"generation_{generation}")
-        os.makedirs(output_folder, exist_ok=True)
+    while True:
+        print(f"Generating generation {generation}")
+        generate_next_generation(base_file, generation)
+        generation += 1
 
-        for child_index in range(num_children):
-            modified_content = []
-            for line in lines:
-                line = line.strip()
-                if line.startswith("- speed"):
-                    value = float(line.split(":")[1].strip())
-                    new_value = value * (1 + random.uniform(-0.2, 0.2))
-                    modified_content.append(f"  - speed: {new_value:.6f}")
-                elif any(line.strip().startswith(param) for param in param_names):
-                    param, value = line.split(":")[0].strip(), float(line.split(":")[1].strip())
-                    new_value = value * (1 + random.uniform(-0.2, 0.2))
-                    modified_content.append(f"    {param}: {new_value:.6f}")
-                else:
-                    modified_content.append(f"    {line}")
+        # Wait for CSV result
+        print(f"Waiting for CSV for generation {generation}")
+        path_to_watch = os.path.dirname(base_file)
+        event_handler = MyHandler()
+        observer = watchdog.observers.Observer()
+        observer.schedule(event_handler, path_to_watch, recursive=False)
+        observer.start()
 
-            output_file_name = f"generation{generation}_modified{child_index}.asset"
-            output_file_path = os.path.join(output_folder, output_file_name)
-            with open(output_file_path, 'w') as file:
-                file.write("\n".join(modified_content))
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
 
-        print(f"Waiting for Unity to run generation {generation} and produce a CSV with results...")
-        watch_for_csv(script_location)
-
-        csv_files = [file for file in os.listdir(script_location) if file.endswith('.csv')]
-        latest_csv = max(csv_files, key=os.path.getctime)
-        print(f"Found latest CSV: {latest_csv}")
-        lap_times = read_csv_laptimes(os.path.join(script_location, latest_csv))
-        best_parents = sorted(lap_times, key=lap_times.get)[:2]
-        print(f"Best parents for next generation are: {best_parents}")
-
-
-# GUI
+# GUI setup
 root = tk.Tk()
 root.title("Genetic Algorithm for Input File Generation")
 
@@ -107,19 +103,15 @@ input_file_label.pack()
 input_file_entry = tk.Entry(root)
 input_file_entry.pack()
 
-
 def browse_input_file():
-    input_file_path = filedialog.askopenfilename()
-    if input_file_path:
-        print(f"Selected input file: {input_file_path}")
-        input_file_entry.delete(0, tk.END)
-        input_file_entry.insert(0, input_file_path)
-
+    file_path = filedialog.askopenfilename()
+    input_file_entry.delete(0, tk.END)
+    input_file_entry.insert(0, file_path)
 
 browse_input_button = tk.Button(root, text="Browse", command=browse_input_file)
 browse_input_button.pack()
 
-run_genetic_algorithm_button = tk.Button(root, text="Run Genetic Algorithm", command=run_genetic_algorithm)
-run_genetic_algorithm_button.pack()
+run_button = tk.Button(root, text="Run Genetic Algorithm", command=run_genetic_algorithm)
+run_button.pack()
 
 root.mainloop()
